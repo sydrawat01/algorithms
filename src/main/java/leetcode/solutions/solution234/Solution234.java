de0e22c0d49197c37c9d3daa8ecdc01310f55db1
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-08-23 23:33:24 -0400

    :rocket: optimize solution #234

--- src/main/java/leetcode/solutions/solution234/Solution234.java
+++ src/main/java/leetcode/solutions/solution234/Solution234.java
@@ -28,6 +28,26 @@ public class Solution234 {
      * @return true or false if palindrome
      */
     public boolean isPalindrome(Node head) {
-        return false;
+        //TODO: Implement optimal solution
+        if (head == null) return true;
+        Node slow = head, fast = head, rev = new Node(head.data);
+
+        while(fast!=null && fast.getNext()!=null) {
+            Node temp = rev;
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+
+            rev = new Node(slow.data);
+            rev.setNext(temp);
+        }
+
+        if (fast == null) rev = rev.getNext();
+
+        while (slow != null) {
+            if (slow.data != rev.data) return false;
+            slow = slow.getNext();
+            rev = rev.getNext();
+        }
+        return true;
     }
 }
--- src/test/java/leetcode/solutions/solution234/Solution234Test.java
+++ src/test/java/leetcode/solutions/solution234/Solution234Test.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class Solution234Test {
     @Test
-    public void palindrome1Check1() {
+    public void palindromeCheckBasic1() {
         LinkedList ll = new LinkedList();
         ll.head = new Node(1, null);
         ll.insertEnd(2);
@@ -20,12 +20,34 @@ public class Solution234Test {
     }
 
     @Test
-    public void palindrome1Check2() {
+    public void palindromeCheckBasic2() {
         LinkedList ll = new LinkedList();
         ll.head = new Node(1, null);
         ll.insertEnd(2);
 
         Solution234 ob = new Solution234();
         assertFalse(ob.isPalindrome1(ll.head));
+    }
+
+    @Test
+    public void palindromeCheckOptimized1() {
+        LinkedList ll = new LinkedList();
+        ll.head = new Node(1, null);
+        ll.insertEnd(2);
+        ll.insertEnd(2);
+        ll.insertEnd(1);
+
+        Solution234 ob = new Solution234();
+        assertTrue(ob.isPalindrome(ll.head));
+    }
+
+    @Test
+    public void palindromeCheckOptimized2() {
+        LinkedList ll = new LinkedList();
+        ll.head = new Node(1, null);
+        ll.insertEnd(2);
+
+        Solution234 ob = new Solution234();
+        assertFalse(ob.isPalindrome(ll.head));
     }
 }
